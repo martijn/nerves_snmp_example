ee90d5ba0fe07b1289a8c1ece8d66f8ae78d521d
Martijn Storck <martijn@lico.nl>
2017-02-10 19:02:42 +0100

    Start new Nerves app, target rpi

---
+++ mix.exs
@@ -0,0 +1,42 @@
+defmodule NervesSnmpExample.Mixfile do
+  use Mix.Project
+
+  @target System.get_env("NERVES_TARGET") || "rpi"
+
+  def project do
+    [app: :nerves_snmp_example,
+     version: "0.1.0",
+     target: @target,
+     archives: [nerves_bootstrap: "~> 0.2.1"],
+     
+     deps_path: "deps/#{@target}",
+     build_path: "_build/#{@target}",
+     
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     aliases: aliases(),
+     deps: deps() ++ system(@target)]
+  end
+
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
+  def application do
+    [mod: {NervesSnmpExample, []},
+     applications: [:logger]]
+  end
+
+  def deps do
+    [{:nerves, "~> 0.4.0"}]
+  end
+
+  def system(target) do
+    [{:"nerves_system_#{target}", ">= 0.0.0"}]
+  end
+
+  def aliases do
+    ["deps.precompile": ["nerves.precompile", "deps.precompile"],
+     "deps.loadpaths":  ["deps.loadpaths", "nerves.loadpaths"]]
+  end
+
+end
---
+++ test/nerves_snmp_example_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesSnmpExampleTest do
+  use ExUnit.Case
+  doctest NervesSnmpExample
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
